f4973d5749656eda39765a14ac4036bc7add9c5f
snadwich <79296594+snadwich01@users.noreply.github.com>
2026-02-25 14:07:43 +0600

    Create insertionSort.java

---
+++ base/insertionSort.java
@@ -0,0 +1,30 @@
+public class insert{
+
+    public static void main(String[] args) {
+        int array[] = {9, 1, 8, 7, 2, 3, 6, 5, 4};
+        for(int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        insertionSort(array);
+
+        for ( int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    public static void insertionSort(int[] array) {
+        for (int i =0; i < array.length; i++) {
+            int temp = array[i];
+            int j = i - 1;
+
+            while(j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = temp;
+        }
+    }
+}
